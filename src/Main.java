ae1989c4b0cc6a0347af02995332b70db8dce45d
Daniel Baird <JamesDanielBaird@gmail.com>
2015-05-21 12:23:47 -0700

    list dependencies in main method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@
  */
 public class Main {
     public static void main(String[] args) {
+
+        // Requires gson-2.3.1
+        // Requires jsoup-1.8.2
+
         MainichiCrawler c = new MainichiCrawler();
         c.run();
     }
